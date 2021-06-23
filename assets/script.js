09fb74d0c9090444f4ea680e22dcd607ebf719ad
TheHebi <nturcotte8@live.com>
2021-06-23 11:20:33 -0700

    working if/else statement to determine win/loss

--- assets/script.js
+++ assets/script.js
@@ -7,7 +7,6 @@
     console.log(userChoice);
     // make usre user only choses valid choice EDGE
 // TODO:random computer choice
-    let result = "";
     // list possible choices
     let compPool = ["r", "p", "s"];
     console.log(compPool);
@@ -25,6 +24,27 @@
     console.log(compChoice)
 // TODO:determine win/loss
     // if/else statement
+    if(userChoice === compChoice){
+        alert("Game is a tie");
+    }else if(userChoice === "s"){
+        if (compChoice === "r"){
+            alert("Computer wins, rock beats scissors");
+        }else{
+            alert("You win, scissors beats paper");
+        }
+    }else if(userChoice === "r"){
+        if(compChoice === "s"){
+            alert("You win, rock beats scissors");
+        }else{
+            alert("Computer wins, paper beats rock");
+        }
+    }else{
+        if(compChoice === "r"){
+            alert("You win, paper beats rock");
+        }else{
+            alert("Computer wins, scissors beats paper")
+        }
+    }
 // TODO:track wins and losses
     // variables for wins/losses/ties
     // show to user end of game
