a79069cc8e149f92094cb113dd3b2ad86fba923c
TheHebi <nturcotte8@live.com>
2021-06-23 10:39:42 -0700

    initial filestructure

---
+++ assets/script.js
@@ -0,0 +1,21 @@
+// rps psuedoe code
+
+// TODO:capture player choice
+    // create pop up prompt for user choice
+    let userChoice = prompt("Choose r, p, or s")
+    // log info into var
+    console.log(userChoice)
+    // make usre user only choses valid choice EDGE
+// TODO:random computer choice
+    // list possible choices
+    // randomly select from list
+    // store in variable
+// TODO:determine win/loss
+    // if/else statement
+// TODO:track wins and losses
+    // variables for wins/losses/ties
+    // show to user end of game
+// TODO:play again?
+    // pop up with true/false options
+    //  if true loop, if false end
+    // recursive or while loop will work here
